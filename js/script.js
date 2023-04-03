0508884c207f484f7db3d4ec5af09347954667a8
MaxReveRxaM <47296839+MaxReveRxaM@users.noreply.github.com>
2023-04-03 08:52:40 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+var burger = document.querySelector("#burger-menu");
+var menu = document.querySelector("#menuLinks");
+
+function showMenu () {
+	burger.classList.toggle("show")
+	menu.classList.toggle("show")
+}
+
+
+
 /*----------------------------------- 
 
 			плавный скролл
@@ -6,41 +16,67 @@
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const blockID = anchor.getAttribute('href').substr(1)
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
 }
 
 
 
 
+/* slider */
 
+const quantity = document.querySelectorAll('.order__item').length;
+const slider = document.querySelector("#slider");
 
+let maxWidth = null;
+let left = null;
+let shift = null;
+let interal = true;
+let cliks = 1;
 
-// Получаем элемент, до которого нужно доскроллить страницу
-const targetElement = document.getElementById('order-list');
-
-// Функция, которую нужно вызвать при достижении элемента
-function myFunction() {
-  console.log('Элемент достигнут!');
+if (window.innerWidth <= 600) {
+	shift = 320;
+	maxWidth = quantity * shift;
+	slider.style.width = maxWidth + 'px';
+} else if (window.innerWidth <= 1050) {
+	shift = 500;
+	maxWidth = quantity * shift;
+	slider.style.width = maxWidth + 'px';
 }
 
-// Обработчик события скролла страницы
-window.addEventListener('scroll', function() {
-  // Получаем положение элемента относительно верхней границы окна просмотра
-  const targetPosition = targetElement.getBoundingClientRect().top;
-  // Получаем высоту окна просмотра
-  const windowHeight = window.innerHeight;
-  // Если элемент находится в видимой области окна просмотра
-  if (targetPosition < windowHeight) {
-    // Вызываем нужную функцию
-    myFunction();
-  }
-});
+function sliderLeft(param) {
+	if (cliks !== 1) {
+		cliks--;
+		interal = false;
+		left = left + shift;
+		slider.style.transform = 'translateX(' + left + 'px' + ')';
+	}
+}
+
+function sliderRight(param) {
+	if (cliks !== quantity) {
+		cliks++;
+		left = left - shift;
+		param == true ? interal = false : interal = true;
+		slider.style.transform = 'translateX(' + left + 'px' + ')';
+	} else if (cliks === quantity) {
+		cliks = 1;
+		left = 0;
+		slider.style.transform = 'translateX(' + 0 + 'px' + ')';
+	}
+}
+
+
+let timerId = setInterval(() => {
+	if (interal === true) {
+		sliderRight();
+	}
+}, 3000);
